492c65f2c810cc0fa974b4f9803417e2b709b0f0
Mauraum <mauraumdev@gmail.com>
2022-02-08 12:33:36 -0300

    inserindo demais entidades

---
+++ src/main/java/br/com/mauraumdev/bolaoCopa/model/BetBoard.java
@@ -0,0 +1,7 @@
+package br.com.mauraumdev.bolaoCopa.model;
+
+public class BetTable {
+    User user;
+    Tournament tournament;
+    Bet bet;
+}
---
+++ src/main/java/br/com/mauraumdev/bolaoCopa/model/Game.java
@@ -0,0 +1,2 @@
+package br.com.mauraumdev.bolaoCopa.model;public class Game {
+}
